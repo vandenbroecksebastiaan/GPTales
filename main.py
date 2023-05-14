eda949e1beb95cac2d352e4972091fcf79973ff6
vandenbroecksebastiaan <van.den.broeck.sebastiaan@gmail.com>
2023-05-14 14:12:16 +0200

    choice in story generation

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from moviepy.editor import ImageSequenceClip, concatenate_audioclips
 from moviepy.audio.io.AudioFileClip import AudioFileClip
 import ffmpeg
 from tqdm import tqdm
-from noisereduce import reduce_noise
 from nltk.tokenize import sent_tokenize
 import openai
 from multiprocessing import Pool
@@ -39,15 +38,19 @@ class GPTales:
 
     def _gpt_call(self, prompt: str, max_tokens: int,
                   model: str = "gpt-3.5-turbo") -> str:
-        response = openai.ChatCompletion.create(
-          model=model,
-          messages=[ {"role": "user", "content": prompt}],
-          max_tokens=max_tokens
-        )
-        return response["choices"][0]["message"]["content"]
+        for idx in range(10):
+            try:
+                response = openai.ChatCompletion.create(
+                    model=model,
+                    messages=[ {"role": "user", "content": prompt}],
+                    max_tokens=max_tokens
+                )
+                return response["choices"][0]["message"]["content"]
+            except openai.error.RateLimitError:
+                print(f"Error in GPT-3 call, trying again... ({idx})")
     
-    def _generate_paragraph(self, story=""):
-        prompt = story_prompt(topics=self.topics, story=story)
+    def _generate_paragraph(self, story="", option=None):
+        prompt = story_prompt(topics=self.topics, story=story, option=option)
         completion = self._gpt_call(prompt=prompt, max_tokens=2000)
         return completion
 
@@ -63,21 +66,34 @@ class GPTales:
         print("\n")
         self.topics = self.topics.split(",")
         self.topics = [topic.strip().lower() for topic in self.topics]
-        
-        first_paragraph = self._generate_paragraph()
-        self.paragraphs.append(first_paragraph)
 
         while True:
-            print("Generating a new paragraph...", end="\r")
-            new_paragraph = self._generate_paragraph(story="".join([i+"\n" for i in self.paragraphs]))
-            self.paragraphs.append(new_paragraph)
-            for i in self.paragraphs: print(i, "\n")
-            next = input("Do you want to continue the story? [y/n] ")
-            if next == "n": break
-            print("\n")
-        
+
+            print("Generating a new paragraph...\n")
+
+            if len(self.paragraphs) == 0:
+                new_paragraph = self._generate_paragraph()
+                self.paragraphs.append(new_paragraph)
+                for i in self.paragraphs: print(i, end="\n")
+                option = input("How do you want to continue the story? [1/2/stop] ")
+                print("\n")
+
+            else:
+
+                new_paragraph = self._generate_paragraph(
+                    story="".join([i+"\n" for i in self.paragraphs]),
+                    option=option
+                )
+                self.paragraphs.append(new_paragraph)
+                for i in self.paragraphs: print(i, end="\n")
+                option = input("How do you want to continue the story? [1/2/stop] ")
+                if option == "stop": break
+                print("\n")
+
         # To ensure that the sentences are properly split into a list
-        story = [sent_tokenize(i) for i in self.paragraphs]
+        story = [i.split("\n\n")[0] for i in self.paragraphs]
+        story = [i.replace('"' ,'') for i in story]
+        story = [sent_tokenize(i) for i in story]
         story = [i for j in story for i in j]
         if "The end." in story: story.remove("The end.")
         self.story = story
@@ -118,28 +134,35 @@ class GPTales:
         
         # TODO: make this into a sliding function to better take into account
         # the context of the story when generating the scenes
+        
+        # TODO: generate an image for a few sentences, intead of just one
 
         print("Generating scenes...")
+        
+        # Make a sliding window of 3 elements that is as long as the original list
+        self.story_clean_sliding = [self.story_clean[i:i+3] for i in range(len(self.story_clean)-4)]
+        self.story_clean_sliding.insert(0, self.story_clean[:2])
+        self.story_clean_sliding.insert(0, self.story_clean[:1])
+        self.story_clean_sliding.append(self.story_clean[-2:])
+        self.story_clean_sliding.append(self.story_clean[-1:])
+        self.story_clean_sliding = ["".join(i) for i in self.story_clean_sliding]
 
-        # Make story into a nested list of 5 elements each and a remainder
-        story_nested = [self.story_clean[i:i+5] for i in range(0, len(self.story_clean), 5)]
+        # Make a nested list of 5 elements each and a remainder
+        story_nested = [self.story_clean_sliding[i:i+5] for i in range(0, len(self.story_clean_sliding), 5)]
 
         def get_scenes(story_subset):
             prompt = sentence_prompt(story_subset)
-            # Loop at least once to ensure that there is a scene for each sentence
+            # Loop at least once
             while True:
                 scenes = self._gpt_call(prompt=prompt, max_tokens=1000)
                 scenes = scenes.split("\n")
                 scenes = [i[11:].replace(".", "").strip() for i in scenes]
                 if len(scenes) == len(story_subset): return scenes
         
-        with Pool(10): scenes = [get_scenes(i) for i in story_nested]
+        with Pool(10): scenes = list(tqdm([get_scenes(i) for i in story_nested], total=len(story_nested)))
         self.scenes = [i for j in scenes for i in j]
-        
-        for i, j, k in zip(self.story, self.story_clean, self.scenes):
-            print(i, "\n", j, "\n", k, "\n\n")
     
-    def story2audio(self, temp):
+    def story2audio(self):
         """Generates audio for each sentence in the story."""
         # TODO: capitalization can be used for emphasis.
         for i in os.listdir("tmp/audio"): os.remove(os.path.join("tmp/audio/", i))
@@ -149,7 +172,7 @@ class GPTales:
                     total=len(self.story))
         for idx, sentence in pbar:
             semantic_tokens = generate_text_semantic(
-                sentence, history_prompt=self.speaker, temp=temp, min_eos_p=0.05
+                sentence, history_prompt=self.speaker, min_eos_p=0.05
             )
             audio_array = semantic_to_waveform(semantic_tokens,
                                                history_prompt=self.speaker)
@@ -162,7 +185,7 @@ class GPTales:
         clean_models()
         _clear_cuda_cache()
 
-    def scene2image(self, steps: int = 20):
+    def scene2image(self, steps: int = 20, sd_post_prompt: str = ""):
         """Uses the stable diffusion 2 model to generate visuals for each
            scene."""
         for i in os.listdir("tmp/images"): os.remove(os.path.join("tmp/images/", i))
@@ -173,12 +196,11 @@ class GPTales:
         model.cuda()
         model.eval()
         sampler = DDIMSampler(model, device="cuda")
-        post_prompt = ", digital art, vivid, highly detailed"
         pbar = tqdm(enumerate(self.scenes), leave=False, desc="Generating images",
                     total=len(self.scenes))
         for idx, scene in pbar:
             scene = scene.replace("[Prompt]", "")
-            generate(prompt=scene+post_prompt, model=model,
+            generate(prompt=scene+sd_post_prompt, model=model,
                      sampler=sampler, steps=steps, path=f"tmp/images/scene_{idx}.png")
     
     def image2video(self):
@@ -195,8 +217,8 @@ class GPTales:
         # TODO: add a some noise to when the images shift
         
         # Get duration of the scene
-        audio_duration = [ffmpeg.probe("tmp/audio/" + i)["format"]["duration"] for i
-                          in audio_files]
+        audio_duration = [ffmpeg.probe("tmp/audio/" + i)["format"]["duration"]
+                          for i in audio_files]
         audio_duration = [float(i) for i in audio_duration]
         image_clip = ImageSequenceClip(image_filenames, durations=audio_duration)
     
@@ -216,12 +238,12 @@ class GPTales:
 
 def main():
     # TODO: add voice modifiers such as [dramatic] or [music] to the story
-    storyteller = GPTales(speaker="en_speaker_0")
+    storyteller = GPTales(speaker="en_speaker_4")
     storyteller.generate_story()
     storyteller.generate_clean_story()
     storyteller.story2scene()
-    storyteller.story2audio(temp=0.5)
-    storyteller.scene2image(steps=50)
+    storyteller.story2audio()
+    storyteller.scene2image(steps=50, sd_post_prompt=", anime, digital art, vivid, highly detailed, 4K")
     storyteller.image2video()
 
 
--- prompts.py
+++ prompts.py
@@ -1,11 +1,10 @@
 from typing import List
 
 
-def sentence_prompt(sentences:  List[str]):
+def sentence_prompt(sentences: List[str]):
     prompt = """
 It is your job to generate a prompt for an image generating model.
 The prompt that you generate should be descriptive and vivid and you can be creative if you want to.
-Don't use any specific details of the story, such as names of characters or places, because the image generating model won't know what they are.
 Only describe the elements in the physical environment that should be present in the image.
 Try to use much keywords as possible. You are discouraged from using full sentences, but are allowed to in case only keywords are not descriptive enough.
 Include all information between brackets, but don't include the brackets themselves.
@@ -14,14 +13,14 @@ Here are some examples, but don't generalise the topics from the examples. Only 
 START EXAMPLE
 
 [Text 0] For months, Sarah had trained for this mission, preparing for every possible scenario and mastering the skills she would need to survive in the harsh environment of space.
-[Text 1] John was a farmer who had lived on the same land his entire life.
+[Text 1] this was just the beginning, but with their new hope, she knew that a brighter future was possible. 
 [Text 2] as the days passed, [a friendly dog with brown and white fur] became more than just a farm dog to [humans with advanced technology and resourceful skills]. 
 [Text 3] [a curious girl with shoulder-length brown hair, wearing a green dress and brown boots] was not afraid of the [a small, gray mouse with big brown eyes] and approached it with a welcoming smile. 
 [Text 4] the [delicate creatures with wings that sparkled in the sunlight and wore dresses made of the softest petals] urged [a young girl with a curious spirit and a brave heart] to approach it, assuring her that it was the key to restoring balance.
 
 
-[Prompt 0] A girl, training, space ship simulator
-[Prompt 1] Strong male, farmer, in front of barn, tractor, farm dog
+[Prompt 0] A white girl with brown hair, practicing and training, space ship simulator
+[Prompt 1] Twinkling lights, a new hope
 [Prompt 2] A friendly dog with brown and white fur, humans with advanced technology
 [Prompt 3] A girl with shoulder-length brown hair wearing a green dress and brown boots, a small gray mouse with big brown eyes and a smile
 [Prompt 4] A sparkling creature wearing a dress, a young girl
@@ -32,7 +31,7 @@ END EXAMPLE
     for idx, sentence in enumerate(sentences): prompt = prompt + f"\n[Text {idx}] {sentence}"
     return prompt
 
-def story_prompt(topics: List[str], story: str = ""):
+def story_prompt(topics: List[str], option: int = None, story: str = ""):
     if len(topics) == 1: topics.append("")
     topics_str = topics[0] + "".join([", " + i for i in topics[1:]])
     if story == "":
@@ -40,25 +39,48 @@ def story_prompt(topics: List[str], story: str = ""):
 You are an extremely creative writer that makes short stories for children.
 You are renowned for your ability to make stories that are both entertaining and educational.
 You add a lot of details about characters and events in the story.
-Remember, this is only the first paragraph of the story, so don't give away too much information.
+It is your task to write the first (long) paragraph of a story about the following topics: {topics_str}.
 Capitalize words that should have an emphasis.
 You don't even have to use all of the topics yet, but you can if you want to!
+After the paragraph, provide two options for the next direction in the story for the reader to choose from.
 Do not end the story. Only write the introduction.
 
-Write the first paragraph for a story about the following topics: {topics_str}.
+BEGIN EXAMPLE
+In the heart of the forest, there lived a BEAR named Benny. Unlike other bears who spent their days searching for honey and catching fish, Benny had a unique HOBBY. He loved to collect trinkets that he found in the forest, from shiny rocks to colorful feathers. One day, Benny had a bright idea. He would open a STORE to showcase his collection to the other animals in the forest. And so, with determination and hard work, BENNY'S BEAR STORE was born. On the GRAND OPENING DAY, all the creatures in the forest were invited to see what Benny had to offer.
+
+[1] A famous animal came to the store and bought a trinket.
+[2] A bad animal came to the store and Benny had to defend the store.
+END EXAMPLE
         """
     else:
         prompt = f"""
 You are an extremely creative writer that makes short stories for children.
 You are renowned for your ability to make stories that are both entertaining and educational.
 You add a lot of details about characters and events in the story.
+It is your task to continue writing the story about the following topics: {topics_str}.
 Capitalize words that should have an emphasis.
 You don't even have to use all of the topics yet, but you can if you want to!
+After the paragraph, provide two options for the next direction in the story for the reader to choose from.
+Do not end the story. Only write the introduction.
 
-Write the next paragraph for a story about the following topics: {topics_str}.
+BEGIN EXAMPLE
+In the vast expanse of space, an ASTRONAUT named Alex floated alone. At first, it was exhilarating to be so far away from everything she had ever known, nothing but the stars and galaxies to keep her company. But as the hours turned into days, and the days into weeks, the excitement turned into fear. Alex's spacecraft malfunctioned, sending her hurtling further into the void. She tried every trick she knew, but nothing seemed to work. Her only comfort in that lonely, endless expanse was her trusty DOG companion, Ranger. Ranger was a smart and loyal dog, trained by Alex to be her co-pilot on her many journeys. As the days passed, Alex began to worry that she would never get home, but Ranger never left her side, even as the oxygen ran lower and lower. 
 
-The previous paragraphs were:
+[1] Alex and Ranger come across a mysterious planet.
+[2] Alex must fix the ship, but Ranger gets separated from her.
+
+Continue the story with option 1.
+
+Alex and Ranger floated through the inky blackness, the only light coming from the distant stars and the occasional flash of a meteor. After weeks of drifting through space, they finally spotted a planet on the horizon. It was small and rocky, with no discernible signs of life, but Alex decided it was worth investigating. She navigated her way towards the planet's surface, with Ranger howling with excitement at the new adventure. As they landed, they noticed something odd about the terrain- it seemed to glow a faint shade of green. Alex reached down to take a sample, but before she could, they were surrounded by a group of strange, insect-like creatures. The creatures chittered and buzzed in excitement, and Alex realized that they were intelligent beings. With Ranger by her side, they began to communicate with the creatures, learning about their society, their customs, and their way of life.
+
+[1] Alex and Ranger learn about the creatures' culture.
+[2] Alex and Ranger must find a way to escape the planet.
+
+END EXAMPLE
+
 {story}
+
+Continue the story with option {option}.
         """
     return prompt
 
